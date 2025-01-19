0edcfb89780502ddff2c6ce15dd8ab4af7346ec1
Rick Dalley <dalley.rick@gmail.com>
2025-01-19 15:37:28 -0700

    Initial Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, Neural Network in Rust!");
+mod model;
+pub mod matrix;
+use model::Model;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut nn = Model::from_json("/Users/richarddalley/Code/Rust/NeuralNetworkRust/data/config.json")?;
+    nn.print_config();
+    nn.load_data()?; // Load data from the CSV file
+    nn.train(true);
+
+    Ok(())
 }
