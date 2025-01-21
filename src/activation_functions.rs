a5a2919a771a534d154db773b5ad63e142701c50
Rick Dalley <dalley.rick@gmail.com>
2025-01-21 15:34:27 -0700

    suppresed unused warning in activation functions

--- src/activation_functions.rs
+++ src/activation_functions.rs
@@ -1,4 +1,4 @@
-
+#[allow(unused)] 
 pub fn sigmoid(x: f64) -> f64 {
     1.0 / (1.0 + (-x).exp())
 }
@@ -9,23 +9,27 @@ pub fn sigmoid_derivative(x: f64) -> f64 {
     s * (1.0 - s)
 }
 
+#[allow(unused)] 
 pub fn swish(x: f64) -> f64 {
     x * sigmoid(x)
 }
 
-// Activation function examples
+#[allow(unused)] 
 pub fn relu(x: f64) -> f64 {
     if x > 0.0 { x } else { 0.0 }
 }
 
+#[allow(unused)] 
 fn leaky_relu(x: f64, alpha: f64) -> f64 {
     if x > 0.0 { x } else { alpha * x }
 }
 
+#[allow(unused)] 
 fn elu(x: f64, alpha: f64) -> f64 {
     if x > 0.0 { x } else { alpha * (x.exp() - 1.0) }
 }
 
+#[allow(unused)] 
 fn softmax(input: &[f64]) -> Vec<f64> {
     let max_input = input.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
     let exp_values: Vec<f64> = input.iter().map(|&x| (x - max_input).exp()).collect();
@@ -33,6 +37,7 @@ fn softmax(input: &[f64]) -> Vec<f64> {
     exp_values.iter().map(|&x| x / sum_exp).collect()
 }
 
+#[allow(unused)] 
 fn tanh(x: f64) -> f64 {
     x.tanh()
 }
